c18a55e8ba97065f7c7a54d8b7727b1cd8302172
nthabi <st10531711@rcconnect.edu.za>
2026-06-10 09:54:41 +0200

    Initial Commit

--- app/src/main/java/com/example/campsitecommander/SplashActivity.kt
+++ app/src/main/java/com/example/campsitecommander/SplashActivity.kt
@@ -1,34 +1,23 @@
-package com.example.weatherapp
+package com.example.campsitecommander
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
-
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-
-        val startButton =
-            findViewById<Button>(R.id.startButton)
-
-        val exitButton =
-            findViewById<Button>(R.id.exitButton)
-
-        startButton.setOnClickListener {
-
-            val intent =
-                Intent(this, MainActivity::class.java)
-
-            startActivity(intent)
-        }
-
-        exitButton.setOnClickListener {
-
-            finishAffinity()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
